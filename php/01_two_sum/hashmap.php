a761459f8fc8e175bbb1d1d5e20f2490d93e1f10
jan <defPelly@proton.me>
2024-04-29 15:21:52 +0200

    init + 01_two_sum

---
+++ php/01_two_sum/hashmap.php
@@ -0,0 +1,22 @@
+<?php
+  class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $map = array();
+        foreach($nums as $i => $v) {
+            $map[$v] = $i;
+        };
+        foreach($nums as $i => $v) {
+            $needle = $target - $v;
+            if (array_key_exists($needle, $map) && $i != $map[$needle]) {
+                return array($map[$needle], $i);
+            };
+        };
+    }
+  }
+?>
