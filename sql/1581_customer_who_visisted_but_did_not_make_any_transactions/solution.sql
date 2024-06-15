361cc7cb11394fb7b5ff8654571bbe60b204c82b
jan <defPelly@proton.me>
2024-06-15 19:57:29 +0200

    Add SQL solution for #1581.

---
+++ sql/1581_customer_who_visisted_but_did_not_make_any_transactions/solution.sql
@@ -0,0 +1,5 @@
+SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans
+FROM Visits 
+LEFT JOIN Transactions ON Transactions.visit_id = Visits.visit_id
+WHERE Transactions.transaction_id IS NULL
+GROUP BY Visits.customer_id
