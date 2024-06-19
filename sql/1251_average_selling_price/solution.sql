7247bc490574b17522cc038d05085fae35434319
jan <defPelly@proton.me>
2024-06-19 17:22:38 +0200

    Add SQL solution for #1251

---
+++ sql/1251_average_selling_price/solution.sql
@@ -0,0 +1,4 @@
+SELECT p.product_id, COALESCE(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold us ON p.product_id = us.product_id AND us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
