93a9d2429d9e0037768dccf5839a513c965e6e42
jan <defPelly@proton.me>
2024-06-24 14:56:31 +0200

    Add SQL solution for #1663

---
+++ sql/1663_percentage_of_users_attended_a_contest/solution.sql
@@ -0,0 +1,5 @@
+-- we use a subquery to query the total number of users 
+SELECT contest_id, ROUND((COUNT(contest_id) / (SELECT COUNT(*) FROM Users) * 100), 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC; 
